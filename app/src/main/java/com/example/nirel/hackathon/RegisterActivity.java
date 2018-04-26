b068f08bd2028737f410090a88a5f91efc3580d3
nirel <nirelb@gmail.com>
2018-04-26 18:27:53 +0300

    Nirel - Added login & sing-up 18:26

--- app/src/main/java/com/example/nirel/hackathon/RegisterActivity.java
+++ app/src/main/java/com/example/nirel/hackathon/RegisterActivity.java
@@ -1,11 +1,29 @@
 package com.example.nirel.hackathon;
 
+import android.content.Intent;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatEditText;
 import android.view.View;
 import android.widget.EditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -18,7 +36,7 @@ public class RegisterActivity extends AppCompatActivity {
 
     public String[] read_user_input(View view){
 
-        EditText usernameWrapper = findViewById(R.id.input_name);
+        final EditText usernameWrapper = findViewById(R.id.input_name);
         final EditText emailWrapper = findViewById(R.id.input_email);
         final EditText passwordWrapper = findViewById(R.id.input_password);
         final EditText addressWrapper = findViewById(R.id.input_address);
@@ -37,8 +55,39 @@ public class RegisterActivity extends AppCompatActivity {
 
     public void register(View view){
 
-        String[] user_input = read_user_input(view);
-        System.out.println(user_input[0]);
+        final String[] user_input = read_user_input(view);
+
+        final Button bRegister = findViewById(R.id.button_signup);
+        bRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (success) {
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                RegisterActivity.this.startActivity(intent);
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                builder.setMessage("Register Failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                RegisterRequest registerRequest = new RegisterRequest(user_input[0], user_input[1], user_input[2], Integer.parseInt(user_input[3]), user_input[4], user_input[5], responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+            }
+        });
 
     }
 }
