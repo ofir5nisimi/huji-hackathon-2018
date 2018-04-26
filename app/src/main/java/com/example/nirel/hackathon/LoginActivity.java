3f16b79297292a9ab7ac01af563a8f7703ff6e5e
nirel <nirelb@gmail.com>
2018-04-26 19:35:22 +0300

    Nirel - Added login & sing-up 18:26

--- app/src/main/java/com/example/nirel/hackathon/LoginActivity.java
+++ app/src/main/java/com/example/nirel/hackathon/LoginActivity.java
@@ -1,11 +1,9 @@
 package com.example.nirel.hackathon;
 
-import android.support.design.widget.TextInputLayout;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.AppCompatEditText;
 import android.view.View;
-import android.widget.EditText;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -13,33 +11,13 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_login);
     }
 
 
-    public String[] read_user_input(View view){
-
-        EditText usernameWrapper = findViewById(R.id.input_name);
-        final EditText emailWrapper = findViewById(R.id.input_email);
-        final EditText passwordWrapper = findViewById(R.id.input_password);
-        final EditText addressWrapper = findViewById(R.id.input_address);
-        final EditText paymentWrapper = findViewById(R.id.input_payment);
-        final EditText sayingWrapper = findViewById(R.id.input_saying);
-
-        String username = usernameWrapper.getText().toString();
-        String email = emailWrapper.getText().toString();
-        String password = passwordWrapper.getText().toString();
-        String address = addressWrapper.getText().toString();
-        String payment = paymentWrapper.getText().toString();
-        String saying = sayingWrapper.getText().toString();
-
-        return new String[] {username, email, password, address, payment, saying};
+    public void go_to_helper(View view){
+        Intent myIntent = new Intent(this, HelperMainActivity.class);
+        this.startActivity(myIntent);
     }
 
-    public void register(View view){
-
-        String[] user_input = read_user_input(view);
-        System.out.println(user_input[0]);
-
-    }
 }
