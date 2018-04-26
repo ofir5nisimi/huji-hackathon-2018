01ecbb230e373ef14e1640967032c9a27b60e64c
nirel <nirelb@gmail.com>
2018-04-27 02:37:20 +0300

    Nirel - Added login & sing-up 01:26

--- app/src/main/java/com/example/nirel/hackathon/ConsumerLoginActivity.java
+++ app/src/main/java/com/example/nirel/hackathon/ConsumerLoginActivity.java
@@ -3,6 +3,7 @@ package com.example.nirel.hackathon;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ConsumerLoginActivity extends AppCompatActivity {
 
@@ -12,8 +13,13 @@ public class ConsumerLoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_consumer_login);
     }
 
-    public void go_to_order(){
+    public void go_to_order(View view){
         Intent order = new Intent(this, OrderCategoriesActivity.class);
-        ConsumerLoginActivity.this.startActivity(order);
+        this.startActivity(order);
+    }
+
+    public void register(View view){
+        Intent myIntent = new Intent(this, RegisterActivity.class);
+        this.startActivity(myIntent);
     }
 }
--- app/src/main/java/com/example/nirel/hackathon/VolunteerLoginActivity.java
+++ app/src/main/java/com/example/nirel/hackathon/VolunteerLoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.nirel.hackathon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class VolunteerLoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class VolunteerLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volunteer_login);
+    }
+
+    public void register(View view){
+        Intent myIntent = new Intent(this, RegisterActivity.class);
+        this.startActivity(myIntent);
+    }
+
+    public void connect(View view){
+        Intent myIntent = new Intent(this, HelperMainActivity.class);
+        this.startActivity(myIntent);
     }
 }
