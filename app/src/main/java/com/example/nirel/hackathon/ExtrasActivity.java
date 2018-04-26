f5e6821d8813e466eaf9d4f6c16f28763b5be8b9
aviel <avieldr@gmail.com>
2018-04-26 19:35:22 +0300

    items progress

---
+++ app/src/main/java/com/example/nirel/hackathon/ExtrasActivity.java
@@ -0,0 +1,14 @@
+package com.example.nirel.hackathon;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ExtrasActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_extras);
+    }
+
+}
--- app/src/main/java/com/example/nirel/hackathon/OrderCategoriesActivity.java
+++ app/src/main/java/com/example/nirel/hackathon/OrderCategoriesActivity.java
@@ -5,16 +5,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class OrderCategoriesActivity {
+public class OrderCategoriesActivity extends  AppCompatActivity {
 
-//    public void bread(View view)
-//    {
-//        Intent bread = new Intent(this, BreadActivity.class);
-//    }
+    public void bread(View view)
+    {
+        Intent bread = new Intent(this, BreadActivity.class);
+        startActivity(bread);
+    }
 
-//    public void milk(View view)
-//    {
-//        Intent milk = new Intent()
-//    }
+    public void diary(View view)
+    {
+        Intent diary = new Intent(this, DiaryActivity.class);
+        startActivity(diary);
+    }
+
+    public void vegetables(View view)
+    {
+        Intent bread = new Intent(this, VegetablesActivity.class);
+        startActivity(bread);
+    }
+
+    public void fruit(View view)
+    {
+        Intent bread = new Intent(this, FruitActivity.class);
+        startActivity(bread);
+    }
+
+    public void house_basics(View view)
+    {
+        Intent house_basics = new Intent(this, HouseBasicsActivity.class);
+        startActivity(house_basics);
+    }
+
+    public void extras(View view)
+    {
+        Intent extras = new Intent(this, ExtrasActivity.class);
+        startActivity(extras);
+    }
+
 
 }
