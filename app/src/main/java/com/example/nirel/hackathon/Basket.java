c28be7067f51490615f991b3574ab60a93fbaf72
aviel <avieldr@gmail.com>
2018-04-26 17:55:53 +0300

    order progress

---
+++ app/src/main/java/com/example/nirel/hackathon/Basket.java
@@ -0,0 +1,13 @@
+package com.example.nirel.hackathon;
+
+public class Basket {
+
+    private String[] basket;
+    private int itemsCount = 0;
+
+    public Basket(){
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/nirel/hackathon/OrderCategoriesActivity.java
@@ -0,0 +1,20 @@
+package com.example.nirel.hackathon;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class OrderCategoriesActivity {
+
+    public void bread(View view)
+    {
+        Intent bread = new Intent(this, BreadActivity.class);
+    }
+
+    public void milk(View view)
+    {
+        Intent milk = new Intent()
+    }
+
+}
